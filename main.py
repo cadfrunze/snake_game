472a40b83d25e0f013e5b762325e1d7b2e98cf39
cadfrunze <78301491+cadfrunze@users.noreply.github.com>
2022-11-03 13:17:19 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+from turtle import Screen
+import time
+from snake import Snake
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.listen()
+screen.bgcolor('black')
+screen.title(titlestring='The Snake game')
+screen.tracer(0)
+sarpele = Snake()
+screen.listen()
+screen.onkey(key='Up', fun=sarpele.move_up)
+screen.onkey(key='Down', fun=sarpele.move_down)
+screen.onkey(key='Left', fun=sarpele.move_left)
+screen.onkey(key='Right', fun=sarpele.move_right)
+jocul = True
+while jocul:
+    screen.update()
+    time.sleep(0.1)
+    sarpele.move()
+
+screen.exitonclick()
