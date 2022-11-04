6afe695c230a8ea52867b37a9cf768484e02279a
cadfrunze <78301491+cadfrunze@users.noreply.github.com>
2022-11-04 16:33:49 +0200

    Create food.py

---
+++ food.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+import random
+
+
+class Food:
+    def __init__(self):
+        self.food = Turtle('circle')
+        self.food.color('blue')
+        self.food.penup()
+        self.food.shapesize(0.5)
+        self.x_cor = 0
+        self.y_cor = 0
+
+    def pozitia(self):
+        x = random.randint(-280, 280)
+        y = random.randint(-280, 280)
+        self.food.goto(x=x, y=y)
+        self.x_cor = self.food.xcor()
+        self.y_cor = self.food.ycor()
+
+    def reseteaza(self):
+        self.food.hideturtle()
+
+
+
