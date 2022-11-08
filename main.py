779341ac10705b6c45116a9c162f0f0eabc9ac1b
cadfrunze <78301491+cadfrunze@users.noreply.github.com>
2022-11-08 13:03:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,30 +19,14 @@ screen.onkey(key='Down', fun=sarpele.move_down)
 screen.onkey(key='Left', fun=sarpele.move_left)
 screen.onkey(key='Right', fun=sarpele.move_right)
 
-mancarea.pozitia()
 
 jocul = True
 while jocul:
     screen.update()
     time.sleep(0.1)
     sarpele.move()
-    adevarat_x = sarpele.x_cor - mancarea.x_cor
-    adevarat_y = mancarea.y_cor - sarpele.y_cor
-    print(f"adevarat_x: {adevarat_x}")
-    print(f"adevarat_y: {adevarat_y}")
-    print(f"scorul: {afisare_scor.scor}")
-    if (adevarat_x <= 12 and adevarat_x >= -12) and (adevarat_y <= 12 and adevarat_y >= -12):
-        afisare_scor.scor += 1
-        screen.tracer()
-        new_element = Turtle('square')
-        new_element.hideturtle()
-        new_element.penup()
-        new_element.color('white')
-        sarpele.segmente.append(new_element)
-        sarpele.segmente[-1].showturtle()
-        screen.update()
-        time.sleep(0.1)
-        mancarea.pozitia()
+
+
 
 
 
