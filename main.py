6903af3dd7429a52a7f88d8b00f2cd9c21392339
cadfrunze <78301491+cadfrunze@users.noreply.github.com>
2022-11-08 21:45:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 import time
 from snake import Snake
 from food import Food
-from score import AfisareScor
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -10,30 +9,20 @@ screen.listen()
 screen.bgcolor('black')
 screen.title(titlestring='The Snake game')
 screen.tracer(0)
+snake_food = Food()
 sarpele = Snake()
-mancarea = Food()
-afisare_scor = AfisareScor()
+
 screen.listen()
 screen.onkey(key='Up', fun=sarpele.move_up)
 screen.onkey(key='Down', fun=sarpele.move_down)
 screen.onkey(key='Left', fun=sarpele.move_left)
 screen.onkey(key='Right', fun=sarpele.move_right)
 
-
 jocul = True
 while jocul:
     screen.update()
     time.sleep(0.1)
     sarpele.move()
-
-
-
-
-
-
-
-
-
-
+    # Intalnirea cu mancarea!
 
 screen.exitonclick()
