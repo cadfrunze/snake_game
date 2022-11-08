cb4d04280b0b095057b18aee8ed608e241625958
cadfrunze <78301491+cadfrunze@users.noreply.github.com>
2022-11-08 13:03:12 +0200

    Update food.py

--- food.py
+++ food.py
@@ -2,24 +2,10 @@ from turtle import Turtle
 import random
 
 
-class Food:
+class Food(Turtle):
     def __init__(self):
-        self.food = Turtle('circle')
-        self.food.color('blue')
-        self.food.penup()
-        self.food.shapesize(0.8)
-        self.x_cor = 0
-        self.y_cor = 0
+        super().__init__()
 
-    def pozitia(self):
-        x = random.randint(-280, 280)
-        y = random.randint(-280, 280)
-        self.food.goto(x=x, y=y)
-        self.x_cor = self.food.xcor()
-        self.y_cor = self.food.ycor()
-
-    def reseteaza(self):
-        self.food.clear()
 
 
 
