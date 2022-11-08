01f28a9f9ff57e3c109d3fb7b73617e43c346100
cadfrunze <78301491+cadfrunze@users.noreply.github.com>
2022-11-08 21:45:09 +0200

    Update food.py

--- food.py
+++ food.py
@@ -5,6 +5,14 @@ import random
 class Food(Turtle):
     def __init__(self):
         super().__init__()
+        self.shape('circle')
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color('blue')
+        self.penup()
+        self.speed('fastest')
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(x=random_x, y=random_y)
 
 
 
