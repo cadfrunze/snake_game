66f09397b9f95b5069554aaee78a778c3fe37809
cadfrunze <marius1890@yahoo.com>
2022-11-17 13:01:57 +0200

    Update

--- score.py
+++ score.py
@@ -1,3 +1,21 @@
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.scor = 0
+        self.color('white')
+        self.penup()
+        self.hideturtle()
+        self.goto(x=0, y=280)
+        self.modify_scor()
+
+    def modify_scor(self):
+        self.clear()
+        super().write(arg=f"Scor: {self.scor}", move=False, align="center", font=('Arial', 14, 'normal'))
+
+
 
 
 
