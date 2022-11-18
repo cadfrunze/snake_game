9091f80b413a2c055ce19d48dd8b4c20338dcc90
cadfrunze <marius1890@yahoo.com>
2022-11-18 13:51:21 +0200

    adaugat metoda daca se musca de coada

--- snake.py
+++ snake.py
@@ -2,7 +2,7 @@ from turtle import Turtle
 
 # Creearea sarpelui
 STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
-MOVE_DISTANCE = 10
+MOVE_DISTANCE = 15
 UP = 90
 DOWN = 270
 LEFT = 180
@@ -19,23 +19,35 @@ class Snake(Turtle):
         self.head = self.segmente[0]
 
     # Locul de unde porneste
-
     def create_snake(self):
         for seg in STARTING_POSITION:
-            new_element = Turtle('square')
-            new_element.penup()
-            new_element.color('white')
-            new_element.goto(seg)
-            self.segmente.append(new_element)
+            self.add_segment(seg)
+
+    def add_segment(self, seg):
+        new_element = Turtle('square')
+        new_element.penup()
+        new_element.color('white')
+        new_element.goto(seg)
+        self.segmente.append(new_element)
+
+    def extented_sarpele(self):
+        # Adauga o piesa la sarpe
+        self.add_segment(self.segmente[-1].position())
 
     # Miscarea in fata, Atentie la capul sarpelui
-
     def move(self):
         for miscare in range(len(self.segmente) - 1, 0, -1):
             new_x = self.segmente[miscare - 1].xcor()
             new_y = self.segmente[miscare - 1].ycor()
             self.segmente[miscare].goto(x=new_x, y=new_y)
         self.head.forward(MOVE_DISTANCE)
+        self.collision()
+
+    def collision(self, ):
+        for num_piesa in range(1, (len(self.segmente) - 1)):
+            if self.head.distance(self.segmente[num_piesa]) < 10:
+                print("Te-ai lovit!")
+                return False
 
     # Directiile
 
